5f5b8e64007ea1f22632bdcf9322ad77ba10bdca
Howon Lee <kck4156@kagamine-rin.com>
2022-10-12 19:01:09 +0900

    리펙토링

--- Kakao Story Manager Plus/LoginManager.cs
+++ Kakao Story Manager Plus/LoginManager.cs
@@ -12,98 +12,97 @@ using WebDriverManager.DriverConfigs.Impl;
 using WebDriverManager;
 using System.IO;
 
-namespace KSMP
+namespace KSMP;
+
+public static class LoginManager
 {
-    public static class LoginManager
+    public static EdgeDriver SeleniumDriver = null;
+
+    private static bool CheckIfElementExists(WebDriver driver, By by)
     {
-        public static EdgeDriver SeleniumDriver = null;
-
-        private static bool CheckIfElementExists(WebDriver driver, By by)
+        try
         {
-            try
-            {
-                driver.FindElement(by);
-                return true;
-            }
-            catch (NoSuchElementException)
-            {
-                return false;
-            }
+            driver.FindElement(by);
+            return true;
         }
-
-        public static bool LoginWithSelenium(string email, string password)
+        catch (NoSuchElementException)
         {
-            var key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Edge\BLBeacon", true);
-            string version = key.GetValue("version") as string;
-            var driverPath = new DriverManager().SetUpDriver(new EdgeConfig(), version);
-            driverPath = Path.GetDirectoryName(driverPath);
+            return false;
+        }
+    }
 
-            var service = EdgeDriverService.CreateDefaultService(driverPath);
-            service.HideCommandPromptWindow = true;
-            service.UseVerboseLogging = true;
+    public static bool LoginWithSelenium(string email, string password)
+    {
+        var key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Edge\BLBeacon", true);
+        string version = key.GetValue("version") as string;
+        var driverPath = new DriverManager().SetUpDriver(new EdgeConfig(), version);
+        driverPath = Path.GetDirectoryName(driverPath);
 
-            var options = new EdgeOptions();
-            //options.AddArgument("headless");
+        var service = EdgeDriverService.CreateDefaultService(driverPath);
+        service.HideCommandPromptWindow = true;
+        service.UseVerboseLogging = true;
 
-            SeleniumDriver = new EdgeDriver(service, options);
-            SeleniumDriver.Navigate().GoToUrl("https://accounts.kakao.com/login/?continue=https://story.kakao.com/");
+        var options = new EdgeOptions();
+        //options.AddArgument("headless");
 
-            try
+        SeleniumDriver = new EdgeDriver(service, options);
+        SeleniumDriver.Navigate().GoToUrl("https://accounts.kakao.com/login/?continue=https://story.kakao.com/");
+
+        try
+        {
+            var isNewLogin = CheckIfElementExists(SeleniumDriver, By.XPath("//*[@id=\"input-loginKey\"]"));
+
+            if (isNewLogin)
             {
-                var isNewLogin = CheckIfElementExists(SeleniumDriver, By.XPath("//*[@id=\"input-loginKey\"]"));
+                var emailBox = SeleniumDriver.FindElement(By.XPath("//*[@id=\"input-loginKey\"]"));
+                emailBox.SendKeys(email);
 
-                if (isNewLogin)
-                {
-                    var emailBox = SeleniumDriver.FindElement(By.XPath("//*[@id=\"input-loginKey\"]"));
-                    emailBox.SendKeys(email);
+                var passwordBox = SeleniumDriver.FindElement(By.XPath("//*[@id=\"input-password\"]"));
+                passwordBox.SendKeys(password);
 
-                    var passwordBox = SeleniumDriver.FindElement(By.XPath("//*[@id=\"input-password\"]"));
-                    passwordBox.SendKeys(password);
-
-                    var loginButton = SeleniumDriver.FindElement(By.XPath("//*[@id=\"mainContent\"]/div/div/form/div[4]/button[1]"));
-                    loginButton.Click();
-                }
-                else
-                {
-                    var emailBox = SeleniumDriver.FindElement(By.XPath("//*[@id=\"id_email_2\"]"));
-                    emailBox.SendKeys(email);
-
-                    var passwordBox = SeleniumDriver.FindElement(By.XPath("//*[@id=\"id_password_3\"]"));
-                    passwordBox.SendKeys(password);
-
-                    var loginButton = SeleniumDriver.FindElement(By.XPath("//*[@id=\"login-form\"]/fieldset/div[8]/button[1]"));
-                    loginButton.Click();
-                }
-
-                var wait = new WebDriverWait(SeleniumDriver, TimeSpan.FromDays(1));
-                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.UrlToBe("https://story.kakao.com/"));
-                var rawCookies = SeleniumDriver.Manage().Cookies.AllCookies;
-
-                bool isSuccess = rawCookies.Any(x => x.Name == "_karmt");
-                if (!isSuccess) return false;
-
-                var cookies = new List<System.Net.Cookie>();
-                var cookieContainer = new CookieContainer();
-                foreach (var rawCookie in rawCookies)
-                {
-                    cookieContainer.Add(new System.Net.Cookie()
-                    {
-                        Name = rawCookie.Name,
-                        Domain = rawCookie.Domain,
-                        Value = rawCookie.Value
-                    });
-                }
-
-                StoryApi.ApiHandler.Init(cookieContainer, cookies);
-                return true;
+                var loginButton = SeleniumDriver.FindElement(By.XPath("//*[@id=\"mainContent\"]/div/div/form/div[4]/button[1]"));
+                loginButton.Click();
             }
-            catch (Exception) { return false; }
-            finally
+            else
             {
-                SeleniumDriver?.Close();
-                SeleniumDriver?.Dispose();
-                SeleniumDriver = null;
+                var emailBox = SeleniumDriver.FindElement(By.XPath("//*[@id=\"id_email_2\"]"));
+                emailBox.SendKeys(email);
+
+                var passwordBox = SeleniumDriver.FindElement(By.XPath("//*[@id=\"id_password_3\"]"));
+                passwordBox.SendKeys(password);
+
+                var loginButton = SeleniumDriver.FindElement(By.XPath("//*[@id=\"login-form\"]/fieldset/div[8]/button[1]"));
+                loginButton.Click();
             }
+
+            var wait = new WebDriverWait(SeleniumDriver, TimeSpan.FromDays(1));
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.UrlToBe("https://story.kakao.com/"));
+            var rawCookies = SeleniumDriver.Manage().Cookies.AllCookies;
+
+            bool isSuccess = rawCookies.Any(x => x.Name == "_karmt");
+            if (!isSuccess) return false;
+
+            var cookies = new List<System.Net.Cookie>();
+            var cookieContainer = new CookieContainer();
+            foreach (var rawCookie in rawCookies)
+            {
+                cookieContainer.Add(new System.Net.Cookie()
+                {
+                    Name = rawCookie.Name,
+                    Domain = rawCookie.Domain,
+                    Value = rawCookie.Value
+                });
+            }
+
+            StoryApi.ApiHandler.Init(cookieContainer, cookies);
+            return true;
+        }
+        catch (Exception) { return false; }
+        finally
+        {
+            SeleniumDriver?.Close();
+            SeleniumDriver?.Dispose();
+            SeleniumDriver = null;
         }
     }
 }
