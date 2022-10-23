fcb0e3c43ddddb857b274f5d7559165eb55f8dff
Howon Lee <kck4156@kagamine-rin.com>
2022-10-23 16:39:22 +0900

    움짤 프사 대응

--- Kakao Story Manager Plus/Extension/ProfileDataExtension.cs
+++ Kakao Story Manager Plus/Extension/ProfileDataExtension.cs
@@ -2,20 +2,20 @@
 
 public static class ProfileDataExtension
 {
-    public static string GetValidUserProfileUrl(this StoryApi.ApiHandler.DataType.UserProfile.ProfileData user) => user.profile_thumbnail_url;
-    public static string GetValidUserProfileUrl(this StoryApi.ApiHandler.DataType.ProfileData.Profile user) => user.profile_thumbnail_url;
-    public static string GetValidUserProfileUrl(this StoryApi.ApiHandler.DataType.FriendData.Profile user) => user.profile_thumbnail_url;
-    public static string GetValidUserProfileUrl(this StoryApi.ApiHandler.DataType.CommentData.Actor user) => user.profile_thumbnail_url;
-    public static string GetValidUserProfileUrl(this StoryApi.ApiHandler.DataType.CommentData.Writer user) => user.profile_thumbnail_url;
-    public static string GetValidUserProfileUrl(this StoryApi.ApiHandler.DataType.ShareData.Actor user) => user.profile_thumbnail_url;
-    public static string GetValidUserProfileUrl(this StoryApi.ApiHandler.DataType.CommentLikes.Actor user) => user.profile_thumbnail_url;
-    public static string GetValidUserProfileUrl(this StoryApi.ApiHandler.DataType.Actor user) => user.profile_thumbnail_url;
-    //public static string GetValidUserProfileUrl(this StoryApi.ApiHandler.DataType.UserProfile.ProfileData user) => user.profile_video_url_square_small ?? user.profile_thumbnail_url;
-    //public static string GetValidUserProfileUrl(this StoryApi.ApiHandler.DataType.ProfileData.Profile user) => user.profile_video_url_square_small ?? user.profile_thumbnail_url;
-    //public static string GetValidUserProfileUrl(this StoryApi.ApiHandler.DataType.FriendData.Profile user) => user.profile_video_url_square_small ?? user.profile_thumbnail_url;
-    //public static string GetValidUserProfileUrl(this StoryApi.ApiHandler.DataType.CommentData.Actor user) => user.profile_video_url_square_small ?? user.profile_thumbnail_url;
-    //public static string GetValidUserProfileUrl(this StoryApi.ApiHandler.DataType.CommentData.Writer user) => user.profile_video_url_square_small ?? user.profile_thumbnail_url;
-    //public static string GetValidUserProfileUrl(this StoryApi.ApiHandler.DataType.ShareData.Actor user) => user.profile_video_url_square_small ?? user.profile_thumbnail_url;
-    //public static string GetValidUserProfileUrl(this StoryApi.ApiHandler.DataType.CommentLikes.Actor user) => user.profile_video_url_square_small ?? user.profile_thumbnail_url;
-    //public static string GetValidUserProfileUrl(this StoryApi.ApiHandler.DataType.Actor user) => user.profile_video_url_square_small ?? user.profile_thumbnail_url;
+    //public static string GetValidUserProfileUrl(this StoryApi.ApiHandler.DataType.UserProfile.ProfileData user) => user.profile_thumbnail_url;
+    //public static string GetValidUserProfileUrl(this StoryApi.ApiHandler.DataType.ProfileData.Profile user) => user.profile_thumbnail_url;
+    //public static string GetValidUserProfileUrl(this StoryApi.ApiHandler.DataType.FriendData.Profile user) => user.profile_thumbnail_url;
+    //public static string GetValidUserProfileUrl(this StoryApi.ApiHandler.DataType.CommentData.Actor user) => user.profile_thumbnail_url;
+    //public static string GetValidUserProfileUrl(this StoryApi.ApiHandler.DataType.CommentData.Writer user) => user.profile_thumbnail_url;
+    //public static string GetValidUserProfileUrl(this StoryApi.ApiHandler.DataType.ShareData.Actor user) => user.profile_thumbnail_url;
+    //public static string GetValidUserProfileUrl(this StoryApi.ApiHandler.DataType.CommentLikes.Actor user) => user.profile_thumbnail_url;
+    //public static string GetValidUserProfileUrl(this StoryApi.ApiHandler.DataType.Actor user) => user.profile_thumbnail_url;
+    public static string GetValidUserProfileUrl(this StoryApi.ApiHandler.DataType.UserProfile.ProfileData user) => user.profile_video_url_square_small ?? user.profile_thumbnail_url;
+    public static string GetValidUserProfileUrl(this StoryApi.ApiHandler.DataType.ProfileData.Profile user) => user.profile_video_url_square_small ?? user.profile_thumbnail_url;
+    public static string GetValidUserProfileUrl(this StoryApi.ApiHandler.DataType.FriendData.Profile user) => user.profile_video_url_square_small ?? user.profile_thumbnail_url;
+    public static string GetValidUserProfileUrl(this StoryApi.ApiHandler.DataType.CommentData.Actor user) => user.profile_video_url_square_small ?? user.profile_thumbnail_url;
+    public static string GetValidUserProfileUrl(this StoryApi.ApiHandler.DataType.CommentData.Writer user) => user.profile_video_url_square_small ?? user.profile_thumbnail_url;
+    public static string GetValidUserProfileUrl(this StoryApi.ApiHandler.DataType.ShareData.Actor user) => user.profile_video_url_square_small ?? user.profile_thumbnail_url;
+    public static string GetValidUserProfileUrl(this StoryApi.ApiHandler.DataType.CommentLikes.Actor user) => user.profile_video_url_square_small ?? user.profile_thumbnail_url;
+    public static string GetValidUserProfileUrl(this StoryApi.ApiHandler.DataType.Actor user) => user.profile_video_url_square_small ?? user.profile_thumbnail_url;
 }
