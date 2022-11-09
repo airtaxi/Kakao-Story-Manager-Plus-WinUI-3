a381f4399035e8c0758382431801b4935bc3218e
Howon Lee <kck4156@kagamine-rin.com>
2022-11-09 15:56:18 +0900

    리펙토링

--- Kakao Story Manager Plus/UserTagManager.cs
+++ Kakao Story Manager Plus/UserTagManager.cs
@@ -16,97 +16,97 @@ using Windows.Security.Authentication.OnlineId;
 using static KSMP.ApiHandler.DataType.FriendData;
 using static KSMP.ClassManager;
 
-namespace KSMP
+namespace KSMP;
+
+public class UserTagManager
 {
-    public class UserTagManager
+    private UserTag _userTag;
+    private string _userId;
+    private static bool s_initialized = false;
+
+    private static ConcurrentBag<UserTagManager> s_instances = new();
+
+    private UserTagManager(UserTag userTag)
     {
-        private UserTag UserTag;
-        private string UserId;
-        private static bool initialized = false;
-
-        private static ConcurrentBag<UserTagManager> s_instances = new();
-
-        private UserTagManager(UserTag userTag)
-        {
-            UserTag = userTag;
-            UserId = userTag.UserId;
-        }
-        private static UserTagManager GenerateUserTagManager(UserTag userTag)
-        {
-            var instance = new UserTagManager(userTag);
-            s_instances.Add(instance);
-            return instance;
-        }
-
-        public void AddNicknameHistory(string nickname)
-        {
-            UserTag.NicknameHistory.Add(nickname);
-            SaveCurrentUserTags();
-        }
-
-        public void SetMemo(string memo)
-        {
-            UserTag.Memo = memo;
-            SaveCurrentUserTags();
-        }
-
-        public void SetCustomNickname(string customNickname)
-        {
-            UserTag.CustomNickname = customNickname;
-            SaveCurrentUserTags();
-        }
-
-        public List<string> GetNicknameHistory() => UserTag.NicknameHistory;
-        public string GetMemo() => UserTag.Memo;
-        public string GetCustomNickname() => UserTag.CustomNickname;
-
-        public static async Task InitializeAsync(List<Profile> profiles)
-        {
-            var savedUserTags = GetSavedUserTags();
-
-            await Task.Run(() =>
-            {
-                Parallel.ForEach(profiles, (profile) =>
-                {
-                    var userTag = savedUserTags.FirstOrDefault(x => x.UserId == profile.id) ?? new()
-                    {
-                        UserId = profile.id
-                    };
-
-                    // Check NicknameHistory
-                    var nicknameHistory = userTag.NicknameHistory;
-                    var nickname = profile.display_name;
-                    if (!nicknameHistory.Contains(nickname)) nicknameHistory.Add(nickname);
-
-                    GenerateUserTagManager(userTag);
-                });
-            });
-            SaveCurrentUserTags();
-            initialized = true;
-        }
-        
-        private static void SaveCurrentUserTags()
-        {
-            var userTags = GetUserTags();
-            Configuration.SetValue("UserTags", userTags);
-        }
-        private static List<UserTag> GetUserTags() => s_instances.Select(x => x.UserTag).ToList();
-        private static List<UserTag> GetSavedUserTags() => (Configuration.GetValue("UserTags") as JArray)?.ToObject<List<UserTag>>() ?? new();
-
-        public static UserTagManager GetUserTagManager(string userId)
-        {
-            if (!initialized) throw new Exception("Not Initialized!");
-
-            var instance = s_instances.FirstOrDefault(x => x.UserId == userId);
-            if (instance == null)
-            {
-                instance = GenerateUserTagManager(new UserTag() { UserId = userId });
-                SaveCurrentUserTags();
-            }
-
-            return instance;
-        }
+        _userTag = userTag;
+        _userId = userTag.UserId;
     }
 
-    public class DuplicateInstanceException : Exception { }
+    public void AddNicknameHistory(string nickname)
+    {
+        _userTag.NicknameHistory.Add(nickname);
+        SaveCurrentUserTags();
+    }
+
+    public void SetMemo(string memo)
+    {
+        _userTag.Memo = memo;
+        SaveCurrentUserTags();
+    }
+
+    public void SetCustomNickname(string customNickname)
+    {
+        _userTag.CustomNickname = customNickname;
+        SaveCurrentUserTags();
+    }
+
+    public List<string> GetNicknameHistory() => _userTag.NicknameHistory;
+    public string GetMemo() => _userTag.Memo;
+    public string GetCustomNickname() => _userTag.CustomNickname;
+
+    public static async Task InitializeAsync(List<Profile> profiles)
+    {
+        var savedUserTags = GetSavedUserTags();
+
+        await Task.Run(() =>
+        {
+            Parallel.ForEach(profiles, (profile) =>
+            {
+                var userTag = savedUserTags.FirstOrDefault(x => x.UserId == profile.id) ?? new()
+                {
+                    UserId = profile.id
+                };
+
+                // Check NicknameHistory
+                var nicknameHistory = userTag.NicknameHistory;
+                var nickname = profile.display_name;
+                if (!nicknameHistory.Contains(nickname)) nicknameHistory.Add(nickname);
+
+                GenerateUserTagManager(userTag);
+            });
+        });
+        SaveCurrentUserTags();
+        s_initialized = true;
+    }
+
+
+    private static UserTagManager GenerateUserTagManager(UserTag userTag)
+    {
+        var instance = new UserTagManager(userTag);
+        s_instances.Add(instance);
+        return instance;
+    }
+
+    private static void SaveCurrentUserTags()
+    {
+        var userTags = GetUserTags();
+        Configuration.SetValue("UserTags", userTags);
+    }
+
+    private static List<UserTag> GetUserTags() => s_instances.Select(x => x._userTag).ToList();
+    private static List<UserTag> GetSavedUserTags() => (Configuration.GetValue("UserTags") as JArray)?.ToObject<List<UserTag>>() ?? new();
+
+    public static UserTagManager GetUserTagManager(string userId)
+    {
+        if (!s_initialized) throw new Exception("Not Initialized!");
+
+        var instance = s_instances.FirstOrDefault(x => x._userId == userId);
+        if (instance == null)
+        {
+            instance = GenerateUserTagManager(new UserTag() { UserId = userId });
+            SaveCurrentUserTags();
+        }
+
+        return instance;
+    }
 }
