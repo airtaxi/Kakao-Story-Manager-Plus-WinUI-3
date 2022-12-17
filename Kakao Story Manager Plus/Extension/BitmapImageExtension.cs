6f9e68b6222ebed71f873f884011799663c12040
Howon Lee <kck4156@kagamine-rin.com>
2022-12-17 17:54:34 +0900

    필요없는 코드 제거

--- Kakao Story Manager Plus/Extension/BitmapImageExtension.cs
+++ Kakao Story Manager Plus/Extension/BitmapImageExtension.cs
@@ -9,12 +9,7 @@ public static class BitmapImageExtension
     public static void DisposeSource(this BitmapImage bitmapImage)
     {
         if (bitmapImage == null) return;
-        try
-        {
-            bitmapImage.UriSource = null;
-            using var ms = new MemoryStream(new byte[] { 0x0 });
-            bitmapImage.SetSource(ms.AsRandomAccessStream());
-        }
+        try { bitmapImage.UriSource = null; }
         catch (Exception) { } //Ignore
     }
 }
