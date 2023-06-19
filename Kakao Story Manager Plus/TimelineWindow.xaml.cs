d17cfd711722fc145470f98511d27f3ff1e673dd
Howon Lee <kck4156@kagamine-rin.com>
2023-06-19 18:52:30 +0900

    포스트 조회 시 기존 창 크기와 최대화 여부 저장하고 불러오는 기능 추가

--- Kakao Story Manager Plus/TimelineWindow.xaml.cs
+++ Kakao Story Manager Plus/TimelineWindow.xaml.cs
@@ -1,9 +1,11 @@
 using KSMP.Controls;
 using KSMP.Utils;
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Windows.Security.Authentication.OnlineId;
 using static KSMP.ApiHandler.DataType.CommentData;
 
 namespace KSMP;
@@ -23,6 +25,14 @@ public sealed partial class TimelineWindow : Window
 		InitializeComponent();
 		WindowHelper.SetupWindowTheme(this);
 
+		var windowState = Configuration.GetValue("timelineWindowState") as WindowState;
+		if(windowState != null)
+		{
+			var wasMaximized = windowState.WasMaxmized;
+			if (wasMaximized) (AppWindow.Presenter as OverlappedPresenter).Maximize();
+			else AppWindow.ResizeClient(new(windowState.Width, windowState.Height));
+		}
+
 		AppWindow.SetIcon(Path.Combine(App.BinaryDirectory, "icon.ico"));
 		Control = new TimelineControl(this, postData, false, true);
 		FrMain.Content = Control;
@@ -32,7 +42,26 @@ public sealed partial class TimelineWindow : Window
 	public static TimelineWindow FindTimelineWindowByPostId(string id) => s_instances.FirstOrDefault(x => x.PostId == id);
 	public static TimelineWindow GetTimelineWindow(PostData postData) => s_instances.FirstOrDefault(x => x.PostId == postData.id) ?? new TimelineWindow(postData);
 
-	private void OnWindowClosed(object sender, WindowEventArgs args) => s_instances.Remove(this);
+	private void OnWindowClosed(object sender, WindowEventArgs args)
+	{
+		var appWindow = AppWindow;
+		var presenter = appWindow?.Presenter as OverlappedPresenter;
+		var isMaximized = presenter?.State == OverlappedPresenterState.Maximized;
+
+		var windowState = Configuration.GetValue("timelineWindowState") as WindowState ?? new();
+
+		windowState.WasMaxmized = isMaximized;
+		if (!isMaximized)
+		{
+			var width = AppWindow.ClientSize.Width;
+			var height = AppWindow.ClientSize.Height;
+			windowState.Width = width;
+			windowState.Height = height;
+		}
+		Configuration.SetValue("timelineWindowState", windowState);
+
+		s_instances.Remove(this);
+	}
 
 	private async void OnPreviewKeyDown(object sender, Microsoft.UI.Xaml.Input.KeyRoutedEventArgs e)
 	{
---
+++ Kakao Story Manager Plus/WindowState.cs
@@ -0,0 +1,8 @@
+﻿namespace KSMP;
+
+public class WindowState
+{
+    public bool WasMaxmized { get; set; }
+    public int Width { get; set; }
+    public int Height { get; set; }
+}
